a1d614405268298cde28db9e43a52d2fb2a86bcd
ddh63 <ddh63@zips.uakron.edu>
2017-04-28 15:56:36 -0400

    Finished project

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -8,7 +8,7 @@ namespace ComicBookGallery.Models
     public class ComicBook
     {
         public int Id { get; set; }
-        public string SeriesTitle { get; set; }
+        public Series Series { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
@@ -18,7 +18,15 @@ namespace ComicBookGallery.Models
         {
             get
             {
-                return SeriesTitle + " #" + IssueNumber;
+                var series = Series;
+                if (series != null)
+                {
+                    return series.Title + " #" + IssueNumber;
+                }
+                else
+                {
+                    return null;
+                }
             }
         }
 
@@ -27,7 +35,15 @@ namespace ComicBookGallery.Models
         {
             get
             {
-                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+                var series = Series;
+                if (series != null)
+                {
+                    return series.Title.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+                }
+                else
+                {
+                    return null;
+                }
             }
         }
     }
